50de2598c305d77a9615d3e91ba96193517431ba
Your Name <admir.celebic@gmx.at>
2021-02-20 17:30:21 +0100

    first commit

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fr. Mag. Ranegger</title>
-    <h1>Hello World!</h1>
 </head>
 <body>
-
+<header>header</header>
+<footer>footer</footer>
 </body>
 </html>
