f81eaebd560b4bfed5c2ac94958e64bebf77d265
Your Name <admir.celebic@gmx.at>
2021-02-20 12:06:58 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Main template file
+ *
+ * @package linda
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fr. Mag. Ranegger</title>
+</head>
+<body>
+
+</body>
+</html>
